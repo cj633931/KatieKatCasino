eb10228174ce676f9b0b6972fd9661e7906cb3f6
Chase <Chase@DSEKTOP-CHASE.home>
2019-11-11 03:59:07 -0600

    Organized navigation menu.

--- view/header.php
+++ view/header.php
@@ -17,12 +17,16 @@ $basedir = array_pop($boom);
         <div id="wrapper">
             <nav role="navigation">
                 <ul>
-                    <li><a href="/<?php echo htmlspecialchars($basedir) ?>">Home</a></li>
-                    <li><a href="/<?php echo htmlspecialchars($basedir) ?>/controller/website?action=view_play">Play</a></li>
-                    <li><a href="/<?php echo htmlspecialchars($basedir) ?>/controller/website?action=view_rules">Rules</a></li>
-                    <li><a href="/<?php echo htmlspecialchars($basedir) ?>/controller/website?action=view_about">About</a></li>
-                    <li><a href="/<?php echo htmlspecialchars($basedir) ?>/controller/website?action=view_profile">Profile</a></li>
                     <li><a href="/<?php echo htmlspecialchars($basedir) ?>/controller/website?action=view_login">Login</a></li>
+                    <li><a href="/<?php echo htmlspecialchars($basedir) ?>/controller/website?action=view_profile">Profile</a></li>
+                    <li><a href="/<?php echo htmlspecialchars($basedir) ?>/controller/website?action=view_about">About</a></li>
+                    <li><a href="/<?php echo htmlspecialchars($basedir) ?>/controller/website?action=view_rules">Rules</a></li>
+                    <li><a href="/<?php echo htmlspecialchars($basedir) ?>/controller/website?action=view_play">Play</a></li>
+                    <li><a href="/<?php echo htmlspecialchars($basedir) ?>">Home</a></li>
+                    
+                    
+                    
+                    
                 </ul>
             </nav>
             <header role="banner">
