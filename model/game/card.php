61ed1ebb3424d0fddf22ccddfbaa58e86313e5b4
cj633931 <cj633931@LR00911.southeast.edu>
2019-11-21 10:52:30 -0600

    Began work on Card class.

--- model/game/card.php
+++ model/game/card.php
@@ -1,8 +1,20 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+class Card {
+    
+    private $suits = array('clubs', 'spades', 'hearts', 'diamonds');
+    private $unicode = array('\u2663','\u2660','\u2665','\u2666');
+    private $names = array('ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king');
+    private $hardValues = array(1,2,3,4,5,6,7,8,9,10,10,10,10);
+    private $softValues = array(11,2,3,4,5,6,7,8,9,10,10,10,10);
+    
+    
+    function __construct($name, $suit) {
+        // If the name is a valid one, assign it.
+        if (in_array($name, $this->names)) {
+            $this->name = $name;
+        }
+        
+        $this->suit = $suit;
+    }
+}
